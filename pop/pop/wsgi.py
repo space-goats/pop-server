d9961a86d83d979cee1e1e2f6e1ed3ea87f5e164
Quasar Jarosz <quasar@ja.rosz.org>
2016-02-05 20:36:22 -0600

    Renamed apps

--- pop/pop/wsgi.py
+++ pop/pop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pop_server project.
+WSGI config for pop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pop_server.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pop.settings")
 
 application = get_wsgi_application()
